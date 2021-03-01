3d4570f216f94d9f79b39bfbced721280b915065
slayersup <55502595+slayersup@users.noreply.github.com>
2021-03-01 22:07:30 +0200

    Initial commit

---
+++ src/test/java/tests/TestBase.java
@@ -0,0 +1,17 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeAll;
+
+import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
+
+public class TestBase {
+
+    @BeforeAll
+    static void setup() {
+        addListener("AllureSelenide", new AllureSelenide());
+        Configuration.startMaximized = true;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud:4444/wd/hub";
+    }
+}
